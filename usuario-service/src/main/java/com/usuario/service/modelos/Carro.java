098c9549d4f9262bc2c8ee583066570a52c352ac
Juan Jose Pena Rivero <jpena77@hotmail.com>
2023-09-28 19:05:37 -0300

    fisrt commit

---
+++ usuario-service/src/main/java/com/usuario/service/modelos/Carro.java
@@ -0,0 +1,28 @@
+package com.usuario.service.modelos;
+
+public class Carro {
+
+	private String Marca;
+	private String Modelo;
+
+	public String getMarca() {
+		return Marca;
+	}
+
+	public void setMarca(String marca) {
+		Marca = marca;
+	}
+
+	public String getModelo() {
+		return Modelo;
+	}
+
+	public void setModelo(String modelo) {
+		Modelo = modelo;
+	}
+
+	public Carro() {
+		super();
+	}
+
+}
